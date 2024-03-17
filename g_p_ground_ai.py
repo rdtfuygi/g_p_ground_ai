df69e10fb655424e597df8f7542ab22e5bc233cf
hh h <572473788@qq.com>
2024-03-17 14:54:22 +0800

    a1

--- g_p_ground_ai.py
+++ g_p_ground_ai.py
@@ -5,45 +5,54 @@ from pipe import pipe
 from torch.utils.tensorboard import SummaryWriter
 
 
-from neural import net
+from neural import actor
 
 
 
 
 if __name__ == '__main__':
-	print(sys.argv)
+	#print(sys.argv)
 
 	name = sys.argv[1]
 
-	file = open(name + '_output.txt', 'w', encoding='utf-8')
+	#file = open(name + '_output.txt', 'w', encoding='utf-8')
 	
 	writer = SummaryWriter('.\\' + name + '_log')
 	
-	ground_net=net(0.3, 0.1)
+	ground_net=actor(1, 1e-3)
 
-	#ground_net.load_state_dict(torch.load('F:\\场地保留\\asd00000050.pth'))
+	#ground_net.load_state_dict(torch.load('F:\\场地保留\\asd00000012.pth'))
 	
 	ground_net.cuda()
 
-	output_pipe=pipe('asd_out') 
-	input_pipe=pipe('asd_in') 
-	callback_pipe=pipe('asd_back') 
+	output_pipe = pipe('asd_out') 
+	input_pipe = pipe('asd_in') 
+	#action_pipe = pipe('asd_act')
+	callback_pipe = pipe('asd_back') 
+	G_pipe = pipe('asd_G')
 
-	i = 0	
+	i = 0
 
 	while True:
 	
 		_, output_ = output_pipe.recept()
 	
-		net_input = torch.tensor(list(output_)).view(-1,935).cuda()
+		net_input = torch.tensor(list(output_)).view(-1,951).cuda()
 	
 		net_output = ground_net.explor(net_input).view(1,-1).tolist()[0]
 
 		input_pipe.send(net_output);
+		
+		#_, action = action_pipe.recept();
 	
-		_, callback_=callback_pipe.recept();
+		_, callback_ = callback_pipe.recept();
+		
+		_, G_ = G_pipe.recept();
+		
+		if i == 0:
+			G_ = [0.0]
 
-		loss = ground_net.learn(list(callback_))
+		loss = ground_net.learn(list(callback_), list(G_))
 	
 		#print(loss);
 		
@@ -51,12 +60,16 @@ if __name__ == '__main__':
 			if(loss != 0.0):
 				writer.add_scalar('loss', loss, i)
 			writer.add_scalar('reward', callback_[0], i)
+			if G_[0] != 0.0:
+				writer.add_scalar('g', G_[0], i)
+			#writer.add_scalar('reward_b', ground_net.reward_bais, i)
+			#writer.add_scalar('reward_l_b', ground_net.reward_l_bais, i)
 
 		if (i % 2000) == 0:
 			for name_, param in ground_net.named_parameters():
 				writer.add_histogram('asd\\/' + name_, param, i)
 				
-		if (i % 5000) == 0:
+		if (i % 2000) == 0:
 			
 			num='{:08d}'.format(int(i / 1000))			
 
