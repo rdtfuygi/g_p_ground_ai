8e8e7d83333a7eda183c7c453c8f81a8bfe63e0a
hh h <572473788@qq.com>
2024-04-05 09:52:49 +0800

    ddpg

--- g_p_ground_ai.py
+++ g_p_ground_ai.py
@@ -1,3 +1,4 @@
+import copy
 from turtle import circle
 import torch
 import torch.cuda
@@ -13,8 +14,6 @@ from neural import share_net
 
 from collections import deque
 
-from torch.cuda.amp import autocast
-from numba import jit
 
 
 
@@ -25,9 +24,12 @@ if __name__ == '__main__':
 	
 	writer = SummaryWriter('.\\' + name + '_log')
 	
-	share = share_net()
-	critic_net = critic(share, 5e-5)
-	actor_net = actor(share, 2, 5e-5)
+	tau = 0.001
+	critic_net = critic(1e-6)
+	actor_net = actor(1, 1e-5)
+	
+	critic_target_net = critic(1e-4)
+	actor_target_net = actor(0.2, 1e-3)
 	
 	# torch.save({
 	# 	'share':share.state_dict(),
@@ -37,19 +39,23 @@ if __name__ == '__main__':
 	# 	'critic_opt':critic_net.opt.state_dict()
 	# 	},'f:\\场地\\' + name + num + '.pth')
 
-	# cp = torch.load('f:\\场地保留\\asd00000003.pth')
-	# share.load_state_dict(cp['share'])
-	# actor_net.load_state_dict(cp['actor'])
-	# critic_net.load_state_dict(cp['critic'])
+	cp = torch.load('f:\\场地保留\\asd00000025.pth')
+	actor_net.load_state_dict(cp['actor'])
+	critic_net.load_state_dict(cp['critic'])
+	actor_target_net.load_state_dict(cp['actor_target'])
+	critic_target_net.load_state_dict(cp['critic_target'])
 
 
-	share.cuda()
 
-	critic_net.share = share
-	actor_net.share = share
+	# critic_net.share = share
+	# actor_net.share = share
 
 	critic_net.cuda()
 	actor_net.cuda()
+	
+	critic_target_net.cuda()
+	actor_target_net.cuda()
+
 
 	output_pipe = pipe('asd_out')
 	input_pipe = pipe('asd_in')
@@ -60,29 +66,29 @@ if __name__ == '__main__':
 
 	i = 0
 	
-	for name_, param in share.named_parameters():
-		if 'fc' in name_:
-			writer.add_histogram('asd_share_fc/' + name_, param, i)
-			#writer.add_histogram('asd_share_fc_grad/' + name_, param.grad, i)
-		else:
-			writer.add_histogram('asd_share_bn/' + name_, param, i)
-			#writer.add_histogram('asd_share_bn_grad/' + name_, param.grad, i)
+	callback_last = 0
+	
+	# for name_, param in share.named_parameters():
+	# 	if 'fc' in name_:
+	# 		writer.add_histogram('asd_share_fc/' + name_, param, i)
+	# 		#writer.add_histogram('asd_share_fc_grad/' + name_, param.grad, i)
+	# 	else:
+	# 		writer.add_histogram('asd_share_bn/' + name_, param, i)
+	# 		#writer.add_histogram('asd_share_bn_grad/' + name_, param.grad, i)
 	for name_, param in actor_net.named_parameters():
-		if 'share' not in name_:
-			if 'fc' in name_:
-				writer.add_histogram('asd_actor_fc/' + name_, param, i)
-				#writer.add_histogram('asd_actor_fc_grad/' + name_, param.grad, i)
-			else:
-				writer.add_histogram('asd_actor_bn/' + name_, param, i)
-				#writer.add_histogram('asd_actor_bn_grad/' + name_, param.grad, i)
+		if 'fc' in name_:
+			writer.add_histogram('asd_actor_fc/' + name_, param, i)
+			#writer.add_histogram('asd_actor_fc_grad/' + name_, param.grad, i)
+		else:
+			writer.add_histogram('asd_actor_bn/' + name_, param, i)
+			#writer.add_histogram('asd_actor_bn_grad/' + name_, param.grad, i)
 	for name_, param in critic_net.named_parameters():
-		if 'share' not in name_:
-			if 'fc' in name_:
-				writer.add_histogram('asd_critic_fc/' + name_, param, i)
-				#writer.add_histogram('asd_critic_fc_grad/' + name_, param.grad, i)
-			else:
-				writer.add_histogram('asd_critic_bn/' + name_, param, i)
-				#writer.add_histogram('asd_critic_bn_grad/' + name_, param.grad, i)
+		if 'fc' in name_:
+			writer.add_histogram('asd_critic_fc/' + name_, param, i)
+			#writer.add_histogram('asd_critic_fc_grad/' + name_, param.grad, i)
+		else:
+			writer.add_histogram('asd_critic_bn/' + name_, param, i)
+			#writer.add_histogram('asd_critic_bn_grad/' + name_, param.grad, i)
 
 	_, output_ = output_pipe.recept()
 
@@ -97,8 +103,11 @@ if __name__ == '__main__':
 
 		net_output = actor_net.explor(net_input)
 			
+		net_output_ = net_output * 2
+		net_output_[:, 40::42] = (net_output[:, 40::42] * 20).round() % 20
+		net_output_[:, 41::42] = (net_output[:, 41::42] * 20).round() % 20
 
-		net_output_ = net_output.view(1,-1).tolist()[0]
+		net_output_ = net_output_.view(1,-1).tolist()[0]
 
 		input_pipe.send(net_output_)
 	
@@ -118,14 +127,15 @@ if __name__ == '__main__':
 
 
 		if G_[0] == 0.0:
-			exp_replay.append((net_input.view(954), callback, net_input_.view(954), net_output.view(336)))
+			exp_replay.append((net_input.view(-1, 954), callback.view(-1,1), net_input_.view(-1, 954), net_output.view(-1, 336)))
+
 		else:
 			writer.add_scalar('g', G_[0], i)
 			
 
 			
-		if len(exp_replay) >= 128:
-			batch = random.sample(exp_replay, 128)
+		if len(exp_replay) >= 512:
+			batch = random.sample(exp_replay, 512)
 			#batch.append(exp_replay[-1])
 			
 			net_input, callback, net_input__, action = zip(*batch)
@@ -135,9 +145,36 @@ if __name__ == '__main__':
 			net_input__ = torch.cat(net_input__).view(-1, 954)
 			action = torch.cat(action).view(-1, 336)
 
-			td_error, critic_loss = critic_net.learn(net_input, callback, net_input__)
-			actor_loss = actor_net.learn(td_error, net_input, action)
+			critic_net.opt.zero_grad()
+			actor_net.opt.zero_grad()
+			
 
+			q = critic_net.forward(torch.cat((net_input, action), dim = 1))
+
+			with torch.no_grad():
+				a_ = actor_target_net.forward(net_input__)
+				q_ = critic_target_net.forward(torch.cat((net_input__, a_), dim = 1))
+
+			td_e = callback / 1000 + 0.9977 * q_ - q
+			
+			critic_loss_ = torch.mean(torch.square(td_e))
+			critic_loss = critic_loss_.item()
+			
+			critic_loss_.backward()
+
+			
+			
+			action = actor_net.forward(net_input)
+			q = critic_net.forward(torch.cat((net_input, action), dim = 1))
+			
+			actor_loss_ = -torch.mean(q)
+			actor_loss = actor_loss_.item()
+			
+			actor_loss_.backward()
+
+			
+			critic_net.opt.step()
+			actor_net.opt.step()
 
 	
 		#print(loss);
@@ -146,41 +183,50 @@ if __name__ == '__main__':
 			#if(loss != 0.0):
 			writer.add_scalar('actor_loss', actor_loss, i)
 			writer.add_scalar('reward', callback_[0], i)
-			writer.add_scalar('critic_loss', critic_loss, i)				
+			writer.add_scalar('critic_loss', critic_loss, i)	
+			
+			if (i % 10) == 9:
+				for param, target_param in zip(actor_net.parameters(), actor_target_net.parameters()):
+					target_param.data.copy_(tau * param.data + (1.0 - tau) * target_param.data)
+    
+				for param, target_param in zip(critic_net.parameters(), critic_target_net.parameters()):
+					target_param.data.copy_(tau * param.data + (1.0 - tau) * target_param.data)
 
 		if (i % 2000) == 1999:
-			for name_, param in share.named_parameters():
-				if 'fc' in name_:
-					writer.add_histogram('asd_share_fc/' + name_, param, i)
-					writer.add_histogram('asd_share_fc_grad/' + name_, param.grad, i)
-				else:
-					writer.add_histogram('asd_share_bn/' + name_, param, i)
-					writer.add_histogram('asd_share_bn_grad/' + name_, param.grad, i)
+		
+
+			# for name_, param in share.named_parameters():
+			# 	if 'fc' in name_:
+			# 		writer.add_histogram('asd_share_fc/' + name_, param, i)
+			# 		writer.add_histogram('asd_share_fc_grad/' + name_, param.grad, i)
+			# 	else:
+			# 		writer.add_histogram('asd_share_bn/' + name_, param, i)
+			# 		writer.add_histogram('asd_share_bn_grad/' + name_, param.grad, i)
 			for name_, param in actor_net.named_parameters():
-				if 'share' not in name_:
-					if 'fc' in name_:
-						writer.add_histogram('asd_actor_fc/' + name_, param, i)
-						writer.add_histogram('asd_actor_fc_grad/' + name_, param.grad, i)
-					else:
-						writer.add_histogram('asd_actor_bn/' + name_, param, i)
-						writer.add_histogram('asd_actor_bn_grad/' + name_, param.grad, i)
+				if 'fc' in name_:
+					writer.add_histogram('asd_actor_fc/' + name_, param, i)
+					writer.add_histogram('asd_actor_fc_grad/' + name_, param.grad, i)
+				else:
+					writer.add_histogram('asd_actor_bn/' + name_, param, i)
+					writer.add_histogram('asd_actor_bn_grad/' + name_, param.grad, i)
 			for name_, param in critic_net.named_parameters():
-				if 'share' not in name_:
-					if 'fc' in name_:
-						writer.add_histogram('asd_critic_fc/' + name_, param, i)
-						writer.add_histogram('asd_critic_fc_grad/' + name_, param.grad, i)
-					else:
-						writer.add_histogram('asd_critic_bn/' + name_, param, i)
-						writer.add_histogram('asd_critic_bn_grad/' + name_, param.grad, i)
+				if 'fc' in name_:
+					writer.add_histogram('asd_critic_fc/' + name_, param, i)
+					writer.add_histogram('asd_critic_fc_grad/' + name_, param.grad, i)
+				else:
+					writer.add_histogram('asd_critic_bn/' + name_, param, i)
+					writer.add_histogram('asd_critic_bn_grad/' + name_, param.grad, i)
 				
 			
 			num='{:08d}'.format(int(i / 1000))
 			
 			
 			torch.save({
-				'share':share.state_dict(),
+				#'share':share.state_dict(),
 				'actor':actor_net.state_dict(),
 				'critic':critic_net.state_dict(),
+				'actor_target':actor_target_net.state_dict(),
+				'critic_target':critic_target_net.state_dict()
 				},'f:\\场地\\' + name + num + '.pth')
 		
 		i += 1
--- neural.py
+++ neural.py
@@ -1,6 +1,4 @@
-from enum import auto
 from pickletools import floatnl
-from tkinter import W
 from turtle import forward
 import torch
 import torch.nn as nn
@@ -8,7 +6,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.cuda
 
-from torch.cuda.amp import autocast, GradScaler
+#from torch.cuda.amp import autocast, GradScaler
 
 
 import math
@@ -76,36 +74,37 @@ class share_net(nn.Module):
 		self.l3k1fc8 = nn.Linear(512, 1)
 		
 		#l4
-		self.l4fc1 = nn.Linear(2763, 2048)
-		self.l4fc2 = nn.Linear(2048, 2048)
-		self.l4fc3 = nn.Linear(2048, 2048)
+		self.l4fc1 = nn.Linear(2763, 4096)
+		self.l4fc2 = nn.Linear(4096, 4096)
+		self.l4fc3 = nn.Linear(4096, 2048)
 		self.l4fc4 = nn.Linear(2048, 2048)
 		self.l4fc5 = nn.Linear(2048, 2048)
-		self.l4fc6 = nn.Linear(2048, 1024)
-		self.l4fc7 = nn.Linear(1024, 1024)
-		self.l4fc8 = nn.Linear(1024, 1024)
-		self.l4fc9 = nn.Linear(1024, 1024)
+		self.l4fc6 = nn.Linear(2048, 2048)
+		self.l4fc7 = nn.Linear(2048, 2048)
+		self.l4fc8 = nn.Linear(2048, 2048)
+		self.l4fc9 = nn.Linear(2048, 2048)
 		
-		self.l4fc10 = nn.Linear(1024, 1024)
+		self.l4fc10 = nn.Linear(2048, 2048)
 		
-		self.l4bn1 = nn.BatchNorm1d(2763, 1e-5)
-		self.l4bn2 = nn.BatchNorm1d(2048, 1e-5)
-		self.l4bn3 = nn.BatchNorm1d(2048, 1e-5)
-		self.l4bn4 = nn.BatchNorm1d(2048, 1e-5)
-		self.l4bn5 = nn.BatchNorm1d(2048, 1e-5)
-		self.l4bn6 = nn.BatchNorm1d(2048, 1e-5)
-		self.l4bn7 = nn.BatchNorm1d(1024, 1e-5)
-		self.l4bn8 = nn.BatchNorm1d(1024, 1e-5)
-		self.l4bn9 = nn.BatchNorm1d(1024, 1e-5)
+		self.l4ln1 = nn.LayerNorm(4096, 1e-5)
+		self.l4ln2 = nn.LayerNorm(4096, 1e-5)
+		self.l4ln3 = nn.LayerNorm(2048, 1e-5)
+		self.l4ln4 = nn.LayerNorm(2048, 1e-5)
+		self.l4ln5 = nn.LayerNorm(2048, 1e-5)
+		self.l4ln6 = nn.LayerNorm(2048, 1e-5)
+		self.l4ln7 = nn.LayerNorm(2048, 1e-5)
+		self.l4ln8 = nn.LayerNorm(2048, 1e-5)
+		self.l4ln9 = nn.LayerNorm(2048, 1e-5)
 		
-		self.l4bn10 = nn.BatchNorm1d(1024, 1e-5)
+		self.l4ln10 = nn.LayerNorm(2048, 1e-5)
 		
 	
 	def forward(self, x:torch.Tensor) ->torch.Tensor:
-			x = x.view(-1, 106)
+		#with autocast(dtype = torch.float16):
+			
+			x = x.reshape(-1, 106)
 		
 			x_ = torch.cat((x[:,95:100], x[:,0:100], x[:,0:5]), dim = 1)
-		#with autocast(dtype = torch.float16):
 			
 			l1k1x = torch.cat((x_[:,5:105:5], x_[:,6:105:5], x_[:,10::5], x_[:,11::5]), dim = 1).view(-1, 4)
 			l1k1x = leaky_relu(self.l1k1fc1.forward(l1k1x))
@@ -182,104 +181,111 @@ class share_net(nn.Module):
 
 			x_ = torch.cat((x_, l3k1x), dim = 1).view(-1, 2763)
 			
-			x_ = self.l4fc1.forward(self.l4bn1.forward(x_))
-			x_ = self.l4fc2.forward(self.l4bn2.forward(x_)) + x_
-			x_ = self.l4fc3.forward(self.l4bn3.forward(x_)) + x_
-			x_ = self.l4fc4.forward(self.l4bn4.forward(x_)) + x_
-			x_ = self.l4fc5.forward(self.l4bn5.forward(x_)) + x_
-			x_ = self.l4fc6.forward(self.l4bn6.forward(x_))
-			x_ = self.l4fc7.forward(self.l4bn7.forward(x_)) + x_
-			x_ = self.l4fc8.forward(self.l4bn8.forward(x_)) + x_
-			x_ = self.l4fc9.forward(self.l4bn9.forward(x_)) + x_
-			x_ = self.l4fc10.forward(self.l4bn10.forward(x_)) + x_
+			x_ = leaky_relu(self.l4ln1.forward(self.l4fc1.forward(x_)))
+			x_ = leaky_relu(self.l4ln2.forward(self.l4fc2.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln3.forward(self.l4fc3.forward(x_)))
+			x_ = leaky_relu(self.l4ln4.forward(self.l4fc4.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln5.forward(self.l4fc5.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln6.forward(self.l4fc6.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln7.forward(self.l4fc7.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln8.forward(self.l4fc8.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln9.forward(self.l4fc9.forward(x_))) + x_
+			x_ = leaky_relu(self.l4ln10.forward(self.l4fc10.forward(x_))) + x_
 			return x_
 
 
 
 class critic(nn.Module):
-	def __init__(self, share:share_net, lr:float = 0.01):
+	def __init__(self, lr:float = 0.01):
 		super(critic, self).__init__()
-		self.share = share
-		self.fc21 = nn.Linear(1024, 1024)
-		self.fc22 = nn.Linear(1024, 1024)
-		self.fc23 = nn.Linear(1024, 1024)
-		self.fc24 = nn.Linear(1024, 1024)
-		# self.fc25 = nn.Linear(1024, 1024)
-		# self.fc26 = nn.Linear(1024, 1024)
-		# self.fc27 = nn.Linear(1024, 1024)
-		# self.fc28 = nn.Linear(1024, 1024)
-		# self.fc29 = nn.Linear(1024, 1024)
+		self.share = share_net()
+		self.fc21 = nn.Linear(2384, 512)
+		self.fc22 = nn.Linear(512, 512)
+		self.fc23 = nn.Linear(512, 512)
+		self.fc24 = nn.Linear(512, 512)
+		self.fc25 = nn.Linear(512, 512)
+		self.fc26 = nn.Linear(512, 512)
+		self.fc27 = nn.Linear(512, 512)
+		self.fc28 = nn.Linear(512, 512)
+		self.fc29 = nn.Linear(512, 512)
 		
-		self.fc30 = nn.Linear(1024, 1)
+		self.fc30 = nn.Linear(512, 1)
 		
-		self.ln21 = nn.LayerNorm(1024, 1e-5)
-		self.ln22 = nn.LayerNorm(1024, 1e-5)
-		self.ln23 = nn.LayerNorm(1024, 1e-5)
-		self.ln24 = nn.LayerNorm(1024, 1e-5)
-		# self.ln25 = nn.LayerNorm(1024, 1e-5)
-		# self.ln26 = nn.LayerNorm(1024, 1e-5)
-		# self.ln27 = nn.LayerNorm(1024, 1e-5)
-		# self.ln28 = nn.LayerNorm(1024, 1e-5)
-		# self.ln29 = nn.LayerNorm(1024, 1e-5)
-		self.ln30 = nn.LayerNorm(1024, 1e-5)
+		self.ln21 = nn.LayerNorm(512, 1e-5)
+		self.ln22 = nn.LayerNorm(512, 1e-5)
+		self.ln23 = nn.LayerNorm(512, 1e-5)
+		self.ln24 = nn.LayerNorm(512, 1e-5)
+		self.ln25 = nn.LayerNorm(512, 1e-5)
+		self.ln26 = nn.LayerNorm(512, 1e-5)
+		self.ln27 = nn.LayerNorm(512, 1e-5)
+		self.ln28 = nn.LayerNorm(512, 1e-5)
+		self.ln29 = nn.LayerNorm(512, 1e-5)
+		#self.ln30 = nn.LayerNorm(512, 1e-5)
 		
-		self.scaler = GradScaler()
+		#self.scaler = GradScaler()
 		
 		self.opt = optim.AdamW(self.parameters(), lr = lr, eps = 1e-5, weight_decay = 1e-5)
 		
 
 	def forward(self, x:torch.Tensor) -> torch.Tensor:
-			x = self.share.forward(x)
+			x_ = self.share.forward(x[:, :954])
 		#with autocast(dtype = torch.float16):
-			x = leaky_relu(self.fc21.forward(self.ln21.forward(x))) + x
-			x = leaky_relu(self.fc22.forward(self.ln22.forward(x))) + x
-			x = leaky_relu(self.fc23.forward(self.ln23.forward(x))) + x
-			x = leaky_relu(self.fc24.forward(self.ln24.forward(x))) + x
-			# x = leaky_relu(self.fc25.forward(self.ln25.forward(x))) + x
-			# x = leaky_relu(self.fc26.forward(self.ln26.forward(x))) + x
-			# x = leaky_relu(self.fc27.forward(self.ln27.forward(x))) + x
-			# x = leaky_relu(self.fc28.forward(self.ln28.forward(x))) + x
-			# x = leaky_relu(self.fc29.forward(self.ln29.forward(x))) + x	
-			x = self.fc30.forward(self.ln30.forward(x))
+			x = torch.cat((x_, x[:, 954:]), dim = 1)
+			# x[:, 994::42] = x[:, 994::42] / 10.0
+			# x[:, 995::42] = x[:, 995::42] / 10.0
+			x = leaky_relu(self.ln21.forward(self.fc21.forward(x)))
+			x = leaky_relu(self.ln22.forward(self.fc22.forward(x))) + x
+			x = leaky_relu(self.ln23.forward(self.fc23.forward(x))) + x
+			x = leaky_relu(self.ln24.forward(self.fc24.forward(x))) + x
+			x = leaky_relu(self.ln25.forward(self.fc25.forward(x))) + x
+			x = leaky_relu(self.ln26.forward(self.fc26.forward(x))) + x
+			x = leaky_relu(self.ln27.forward(self.fc27.forward(x))) + x
+			x = leaky_relu(self.ln28.forward(self.fc28.forward(x))) + x
+			x = leaky_relu(self.ln29.forward(self.fc29.forward(x))) + x
+			x = self.fc30.forward(x)
 			return x
 	
 
-	def learn(self, s:torch.Tensor, r:torch.Tensor, s_new:torch.Tensor) -> torch.Tensor:
+	# def learn(self, s:torch.Tensor, a:torch.Tensor, r:torch.Tensor, t:critic, s_new:torch.Tensor, a_new:torch.Tensor):
 		
-		self.train()
-		self.opt.zero_grad()		
+	# 	self.train()
+	# 	#self.opt.zero_grad()
 
-		#print(s.size())
+	# 	#print(s.size())
 
-		v = self.forward(s)
-		v_new = self.forward(s_new)
+	# 	x = torch.cat((s, a), dim = 1)
 
-		#with autocast(dtype = torch.bfloat16):
-		td_e = 0.99 * v_new + r - v
-		loss = torch.mean(torch.square(td_e))
+	# 	v = self.forward(x)
 		
-		# self.scaler.scale(loss).backward()
-		# self.scaler.unscale_(self.opt)
-		# torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
-		# self.scaler.step(self.opt)
-		# self.scaler.update()
+	# 	x_new = torch.cat((s_new, a_new), dim = 1)
+	# 	v_new = t.forward(x_new)
 
-		loss.backward()
-		torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
-		self.opt.step()
+	# 	#with autocast(dtype = torch.bfloat16):
+	# 	td_e = 0.99 * v_new + r - v
+	# 	loss = torch.mean(torch.square(td_e))
+		
+	# 	# self.scaler.scale(loss).backward()
+	# 	# self.scaler.unscale_(self.opt)
+	# 	# torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
+	# 	# self.scaler.step(self.opt)
+	# 	# self.scaler.update()
+
+	# 	loss.backward()
+	# 	torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
+	# 	#self.opt.step()
 
 
-		return (td_e.detach(), loss.item())
+	# 	return (td_e.detach(), loss.item())
 
 
 class actor(nn.Module):
-	def __init__(self, share:share_net, noise_std:float = 0.1, lr:float = 0.01):
+	def __init__(self, noise_std:float = 0.1, lr:float = 0.01):
 		super(actor, self).__init__()
 		
-		self.share = share
+		self.share = share_net()
 
 
-		self.fc21 = nn.Linear(1024, 1024)
+		self.fc21 = nn.Linear(2048, 1024)
 		self.fc22 = nn.Linear(1024, 1024)
 		self.fc23 = nn.Linear(1024, 1024)
 		self.fc24 = nn.Linear(1024, 1024)
@@ -287,21 +293,21 @@ class actor(nn.Module):
 		self.fc26 = nn.Linear(1024, 1024)
 		self.fc27 = nn.Linear(1024, 1024)
 		self.fc28 = nn.Linear(1024, 1024)
-		self.fc29 = nn.Linear(1024, 1024)
+		self.fc29 = nn.Linear(1024, 512)
 		
-		# self.fc30 = nn.Linear(1024, 1024)
-		# self.fc31 = nn.Linear(1024, 1024)
-		# self.fc32 = nn.Linear(1024, 1024)
-		# self.fc33 = nn.Linear(1024, 1024)
-		# self.fc34 = nn.Linear(1024, 1024)
-		# self.fc35 = nn.Linear(1024, 1024)
-		# self.fc36 = nn.Linear(1024, 1024)
-		# self.fc37 = nn.Linear(1024, 1024)
-		# self.fc38 = nn.Linear(1024, 1024)
-		# self.fc39 = nn.Linear(1024, 1024)
+		self.fc30 = nn.Linear(512, 512)
+		self.fc31 = nn.Linear(512, 512)
+		self.fc32 = nn.Linear(512, 512)
+		self.fc33 = nn.Linear(512, 512)
+		self.fc34 = nn.Linear(512, 512)
+		self.fc35 = nn.Linear(512, 512)
+		self.fc36 = nn.Linear(512, 512)
+		self.fc37 = nn.Linear(512, 512)
+		self.fc38 = nn.Linear(512, 512)
+		self.fc39 = nn.Linear(512, 512)
 
-		self.fc40 = nn.Linear(1024, 336)
-		
+		self.fc40 = nn.Linear(512, 336)
+		#torch.nn.init.uniform_(self.fc40.weight, -0.001, 0.001)
 
 
 		self.ln21 = nn.LayerNorm(1024, 1e-5)
@@ -312,20 +318,20 @@ class actor(nn.Module):
 		self.ln26 = nn.LayerNorm(1024, 1e-5)
 		self.ln27 = nn.LayerNorm(1024, 1e-5)
 		self.ln28 = nn.LayerNorm(1024, 1e-5)
-		self.ln29 = nn.LayerNorm(1024, 1e-5)
+		self.ln29 = nn.LayerNorm(512, 1e-5)
 		
-		# self.ln30 = nn.LayerNorm(1024, 1e-5)
-		# self.ln31 = nn.LayerNorm(1024, 1e-5)
-		# self.ln32 = nn.LayerNorm(1024, 1e-5)
-		# self.ln33 = nn.LayerNorm(1024, 1e-5)
-		# self.ln34 = nn.LayerNorm(1024, 1e-5)
-		# self.ln35 = nn.LayerNorm(1024, 1e-5)
-		# self.ln36 = nn.LayerNorm(1024, 1e-5)
-		# self.ln37 = nn.LayerNorm(1024, 1e-5)
-		# self.ln38 = nn.LayerNorm(1024, 1e-5)
-		# self.ln39 = nn.LayerNorm(1024, 1e-5)
+		self.ln30 = nn.LayerNorm(512, 1e-5)
+		self.ln31 = nn.LayerNorm(512, 1e-5)
+		self.ln32 = nn.LayerNorm(512, 1e-5)
+		self.ln33 = nn.LayerNorm(512, 1e-5)
+		self.ln34 = nn.LayerNorm(512, 1e-5)
+		self.ln35 = nn.LayerNorm(512, 1e-5)
+		self.ln36 = nn.LayerNorm(512, 1e-5)
+		self.ln37 = nn.LayerNorm(512, 1e-5)
+		self.ln38 = nn.LayerNorm(512, 1e-5)
+		self.ln39 = nn.LayerNorm(512, 1e-5)
 
-		self.ln40 = nn.LayerNorm(1024, 1e-5)
+		self.ln40 = nn.LayerNorm(336, 1e-5)
 
 
 		self.noise_std = noise_std
@@ -336,8 +342,8 @@ class actor(nn.Module):
 		#self.exp_replay = deque(maxlen = 2048)
 		# self.state = torch.Tensor()
 		# self.action = torch.Tensor()
-		self.scaler = GradScaler()
-		self.opt = optim.AdamW(self.parameters(), lr = lr, eps = 1e-5, weight_decay = 1e-5)
+		#self.scaler = GradScaler()
+		self.opt = optim.AdamW(self.parameters(), lr = lr, eps = 1e-5, weight_decay = 1e-9)
 		
 		self.loss_bais = 0.91893853320467274178032973640562 + math.log(self.noise_std)
 		#self.reward_bais = 0.0
@@ -345,39 +351,35 @@ class actor(nn.Module):
 	
 
 	def forward(self, x:torch.Tensor) -> torch.Tensor:
-		x = self.share.forward(x)
+			x = self.share.forward(x)
 		
-		with autocast(dtype = torch.float16):
-			x = leaky_relu(self.fc21.forward(self.ln21.forward(x))) + x
-			x = leaky_relu(self.fc22.forward(self.ln22.forward(x))) + x
-			x = leaky_relu(self.fc23.forward(self.ln23.forward(x))) + x
-			x = leaky_relu(self.fc24.forward(self.ln24.forward(x))) + x
-			x = leaky_relu(self.fc25.forward(self.ln25.forward(x))) + x
-			x = leaky_relu(self.fc26.forward(self.ln26.forward(x))) + x
-			x = leaky_relu(self.fc27.forward(self.ln27.forward(x))) + x
-			x = leaky_relu(self.fc28.forward(self.ln28.forward(x))) + x
-			x = leaky_relu(self.fc29.forward(self.ln29.forward(x))) + x	
+		#with autocast(dtype = torch.float16):
+			x = leaky_relu(self.ln21.forward(self.fc21.forward(x)))
+			x = leaky_relu(self.ln22.forward(self.fc22.forward(x))) + x
+			x = leaky_relu(self.ln23.forward(self.fc23.forward(x))) + x
+			x = leaky_relu(self.ln24.forward(self.fc24.forward(x))) + x
+			x = leaky_relu(self.ln25.forward(self.fc25.forward(x))) + x
+			x = leaky_relu(self.ln26.forward(self.fc26.forward(x))) + x
+			x = leaky_relu(self.ln27.forward(self.fc27.forward(x))) + x
+			x = leaky_relu(self.ln28.forward(self.fc28.forward(x))) + x
+			x = leaky_relu(self.ln29.forward(self.fc29.forward(x)))
+			x = leaky_relu(self.ln30.forward(self.fc30.forward(x))) + x
+			x = leaky_relu(self.ln31.forward(self.fc31.forward(x))) + x
+			x = leaky_relu(self.ln32.forward(self.fc32.forward(x))) + x
+			x = leaky_relu(self.ln33.forward(self.fc33.forward(x))) + x
+			x = leaky_relu(self.ln34.forward(self.fc34.forward(x))) + x
+			x = leaky_relu(self.ln35.forward(self.fc35.forward(x))) + x
+			x = leaky_relu(self.ln36.forward(self.fc36.forward(x))) + x
+			x = leaky_relu(self.ln37.forward(self.fc37.forward(x))) + x
+			x = leaky_relu(self.ln38.forward(self.fc38.forward(x))) + x
+			x = leaky_relu(self.ln39.forward(self.fc39.forward(x))) + x
 
-			# x = leaky_relu(self.fc30.forward(self.ln30.forward(x))) + x
-			# x = leaky_relu(self.fc31.forward(self.ln31.forward(x))) + x
-			# x = leaky_relu(self.fc32.forward(self.ln32.forward(x))) + x
-			# x = leaky_relu(self.fc33.forward(self.ln33.forward(x))) + x
-			# x = leaky_relu(self.fc34.forward(self.ln34.forward(x))) + x
+			x = torch.tanh(self.ln40.forward(self.fc40.forward(x)))
 
-			#x = x.view(-1, 128)	
-
-			# x = leaky_relu(self.fc35.forward(self.ln35.forward(x))) + x
-			# x = leaky_relu(self.fc36.forward(self.ln36.forward(x))) + x
-			# x = leaky_relu(self.fc37.forward(self.ln37.forward(x))) + x
-			# x = leaky_relu(self.fc38.forward(self.ln38.forward(x))) + x
-			# x = leaky_relu(self.fc39.forward(self.ln39.forward(x))) + x
-
-			x = self.fc40.forward(self.ln40.forward(x))
-
-			x_ = torch.tanh(x) * 10
-			x_[:, 40::42] = x[:, 40::42]
-			x_[:, 41::42] = x[:, 41::42]
-		return x_
+			# x_ = torch.tanh(x) * 2
+			# x_[:, 40::42] = x[:, 40::42] * 20 % 20
+			# x_[:, 41::42] = x[:, 41::42] * 20 % 20
+			return x
 	
 
 	def explor(self, x:torch.Tensor) -> torch.Tensor:
@@ -385,38 +387,40 @@ class actor(nn.Module):
 			self.eval()
 			#self.state = x.detach().clone()
 			action = self.forward(x)
-			#with autocast(dtype = torch.float16):
+			
+			# with autocast(dtype = torch.float16):
+				
 			noise = torch.normal(mean = 0, std = self.noise_std, size = action.size()).cuda()
 			
 			action_ = (action + noise)
 
-			action_[:, 40::42] = (torch.rand(action[:, 40::42].size()).cuda() * self.var_range - 0.5 * self.var_range + action[:, 40::42]).round()
-			action_[:, 41::42] = (torch.rand(action[:, 41::42].size()).cuda() * self.var_range - 0.5 * self.var_range + action[:, 41::42]).round()
+			# action_[:, 40::42] = (torch.rand(action[:, 40::42].size()).cuda() * 2 * self.var_range - 1 * self.var_range + action[:, 40::42]).round() % 20
+			# action_[:, 41::42] = (torch.rand(action[:, 41::42].size()).cuda() * 2 * self.var_range - 1 * self.var_range + action[:, 41::42]).round() % 20
 
 		return action_.detach()
 	
 
-	def learn(self, td_e:torch.Tensor, state:torch.Tensor, action:torch.Tensor) -> float:
+	# def learn(self, td_e:torch.Tensor, state:torch.Tensor, action:torch.Tensor) -> float:
 
-		self.train()
-		self.opt.zero_grad()
+	# 	self.train()
+	# 	#self.opt.zero_grad()
 
-		output = self.forward(state)		
+	# 	output = self.forward(state)		
 
-		#with autocast(dtype = torch.bfloat16):
-		action_probs = torch.distributions.Normal(output, self.noise_std).log_prob(action)
+	# 	#with autocast(dtype = torch.bfloat16):
+	# 	action_probs = torch.distributions.Normal(output, self.noise_std).log_prob(action)
 			
-		with torch.no_grad():
-			td_e_ = F.normalize(td_e - torch.mean(td_e), dim = 0)
-		loss = -torch.mean(action_probs * td_e_)
+	# 	with torch.no_grad():
+	# 		td_e_ = F.normalize(td_e - torch.mean(td_e), dim = 0)
+	# 	loss = -torch.mean(action_probs * td_e_)
 
-		# self.scaler.scale(loss).backward()
-		# self.scaler.unscale_(self.opt)
-		# torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
-		# self.scaler.step(self.opt)
-		# self.scaler.update()
+	# 	# self.scaler.scale(loss).backward()
+	# 	# self.scaler.unscale_(self.opt)
+	# 	# torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
+	# 	# self.scaler.step(self.opt)
+	# 	# self.scaler.update()
 
-		loss.backward()
-		torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
-		self.opt.step()
-		return loss.item()
+	# 	loss.backward()
+	# 	torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
+	# 	#self.opt.step()
+	# 	return loss.item()
